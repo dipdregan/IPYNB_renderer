3c959a0a1995b3db71ec45e975139ed942553645
Dipendra Pratap <dipendrapratap155@gmail.comgit config --global user.email dipendrapratap155@gmail.com>
2023-02-22 20:41:45 -0800

    setup of local pckg

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md","r",encoding="utf-8") as f:
+    long_description = f.read()
+
+__version__ = "0.0.0"
+
+REPO_NAME ="IPYNB_renderer"
+AUTHOR_USER_NAME = "dipdregan"
+SRC_REPO = "IPYNB"
+AUTHOR_EMAIL = "dipendrapratap155@gmail.com"
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package",
+    long_description=long_description
+    long_description_content="test/markdown",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls = {
+        "Bug Tracker":f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+    },
+    package_dir=setuptools.find_packages(where = "src")
+    packages=setuptools.find_packages(where = "src")
+)
